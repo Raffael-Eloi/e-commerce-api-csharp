f0731c7e4693ec7d1aa247ef1fb73f0a18d950bd
Raffael Silva <raffaeleloi121@gmail.com>
2022-10-09 08:51:47 -0300

    add all routes of products

--- src/Api/Controllers/ProdutosController.cs
+++ src/Api/Controllers/ProdutosController.cs
@@ -10,5 +10,35 @@ namespace Api.Controllers
 		public void VincularPromocaoAoProduto(int idDaPromocao, int idDoProduto)
 		{
 		}
+
+		[HttpGet("todos")]
+		public string TodosOsProdutos()
+		{
+			return "Aqui vai ser o get all";
+		}
+
+		[HttpGet("detalhes/{id:int}")]
+		public string Detalhes(int id)
+		{
+			return $"Aqui vai ser o get do produto do id {id}";
+		}
+
+		[HttpPost("novo")]
+		public string NovoProduto()
+		{
+			return "Aqui vai criar um novo Produto";
+		}
+
+		[HttpPut("editar/{id:int}")]
+		public string EditarProduto(int id)
+		{
+			return $"Aqui vai ser o editar do produto do id {id}";
+		}
+
+		[HttpDelete("excluir/{id:int}")]
+		public string ExcluirProduto(int id)
+		{
+			return $"Aqui vai ser o excluir do produto do id {id}";
+		}
 	}
 }
